1858bd51fa7119a8ad9742c8892b6b4e0092f083
laughing-nerd <riturajpaul99@gmail.com>
2023-08-16 00:03:28 +0530

    Added a keybind for formatting

--- lua/user/plugins/lsp.lua
+++ lua/user/plugins/lsp.lua
@@ -9,6 +9,7 @@ lsp.ensure_installed({
 	'eslint',
 	'pyright',
   'rust_analyzer',
+  'gopls'
 })
 -- Fix Undefined global 'vim'
 lsp.nvim_workspace()
@@ -37,10 +38,10 @@ lsp.setup_nvim_cmp({
 lsp.set_preferences({
     suggest_lsp_servers = false,
     sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
+        error = '',
+        warn = '',
+        hint = '',
+        info = ''
     }
 })
 
@@ -57,6 +58,7 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set({ "n", "i" }, '<C-f>', function() vim.lsp.buf.format({async = false, timeout_ms = 10000}) end, opts)
 end)
 
 lsp.setup()
